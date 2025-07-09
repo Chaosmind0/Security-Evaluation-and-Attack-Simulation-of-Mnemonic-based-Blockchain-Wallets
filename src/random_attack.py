598bc6a213759b2b2be0d67cdc10bd97350cb573
Qikai Zhou <2616452q@student.gla.ac.uk>
2025-07-09 16:26:33 +0100

    create Random brute-force attack strategy file

---
+++ src/random_attack.py
@@ -0,0 +1,12 @@
+from base_strategy import BruteForceAttackStrategy
+from simulate_brute_force_attack import simulate_brute_force_attack
+
+class RandomAttackStrategy(BruteForceAttackStrategy):
+    """
+    
+    Random brute-force attack strategy.
+
+    """
+    
+    def run(self, **kwargs) -> dict:
+        return simulate_brute_force_attack(**kwargs)
