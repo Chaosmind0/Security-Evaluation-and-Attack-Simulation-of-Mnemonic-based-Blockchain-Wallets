23508d6de36a2c4fb5748f6580f3a66ef653bc6a
Qikai Zhou <2616452q@student.gla.ac.uk>
2025-07-17 19:01:21 +0100

    simplify resource_path

--- src/utils.py
+++ src/utils.py
@@ -2,27 +2,21 @@ import os
 import sys
 import pkgutil
 
-def resource_path(relative_path: str):
+def resource_path(relative_path: str) -> str:
     """ 
     
     Get absolute path to resource, works for dev and for PyInstaller 
+
+    parameters:
+        relative_path: path relative to the current module 
+    
+    returns:
+        absolute path to the resource 
     
     """
     
     try:
         base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
-    
-    full_path = os.path.join(base_path, relative_path)
-
-    if not os.path.exists(full_path):
-        data = pkgutil.get_data('', relative_path)
-        if data:
-            temp_path = os.path.join("/tmp", relative_path)
-            os.makedirs(os.path.dirname(temp_path), exist_ok=True)
-            with open(temp_path, "wb") as f:
-                f.write(data)
-            return temp_path
-
-    return full_path
+    except AttributeError:
+        base_path = os.path.abspath(os.path.dirname(__file__))
+    return os.path.join(base_path, relative_path)
